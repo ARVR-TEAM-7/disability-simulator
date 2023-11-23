702a80bc30477bdb02d543ef5977458c1f6c9ace
cleeuh <clee48@hawaii.edu>
2023-11-22 17:01:35 -1000

    added timer and scoreboard/ score counter

--- Assets/Scripts/DisabilityEvent.cs
+++ Assets/Scripts/DisabilityEvent.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DisabilityEvent : MonoBehaviour
 {
+    public static DisabilityEvent instance { get; private set; }
+
+    [Header("Timers")]
     public float sessionTimeInSeconds = 30f;
     public float switchDisabilitiesInSeconds = 10f;
+
+    [Header("Components")]
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI timer;
+
+    private int scoreCount;
 
     private float sessionTimeNow = 0f;
     private float timeSinceLastSession = 0f;
@@ -18,9 +28,22 @@ public class DisabilityEvent : MonoBehaviour
     private DisabilityManager disabilityManager;
     void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
         disabilityManager = GetComponent<DisabilityManager>();
     }
 
+    public void IncrementScore(int increment)
+    {
+        scoreCount += increment;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -41,11 +64,16 @@ public class DisabilityEvent : MonoBehaviour
             timeTillNewDisability = switchDisabilitiesInSeconds;
             disabilityManager.SetRandomDisability();
         }
+
+        score.text = scoreCount.ToString();
+        timer.text = string.Format("{0:0.0}", timeTillNewSession - sessionTimeNow);
+
     }
 
     void StartNewSession()
     {
         Debug.Log("New Session");
+        scoreCount = 0;
     }
 
 }

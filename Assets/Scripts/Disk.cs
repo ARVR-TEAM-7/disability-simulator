4ab707f2fe90b6708177303e6f752dd5c5062fbd
Giorgio <tran.giorgio@gmail.com>
2023-11-29 14:59:27 -1000

    Add double wield and modify disk ejection

--- Assets/Scripts/Disk.cs
+++ Assets/Scripts/Disk.cs
@@ -4,48 +4,48 @@ using UnityEngine;
 
 public class Disk : MonoBehaviour
 {
-    public GameObject disk;
-    public GameObject diskShattered;
-    public Color[] diskColors;
+  public GameObject disk;
+  public GameObject diskShattered;
+  public Color[] diskColors;
 
-    public AudioSource[] shatterSounds; // expected length = 3
+  public AudioSource[] shatterSounds; // expected length = 3
 
-    private int scoreIncrementation = 0;
+  private int scoreIncrementation = 0;
 
-    public void SetRandomColor()
+  public void SetRandomColor()
+  {
+    Material diskMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+
+    int randomNumber = Random.Range(0, 4);
+    if (randomNumber == 0)
     {
-        Material diskMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
-
-        int randomNumber = Random.Range(0, 2);
-        if (randomNumber == 0)
-        {
-            diskMaterial.color = Color.red;
-            scoreIncrementation = -2;
-        }
-        else
-        {
-            diskMaterial.color = Color.green;
-            scoreIncrementation = 1;
-        }
-
-        diskMaterial.SetFloat("_Metallic", 1f);
-        diskMaterial.SetFloat("_Glossiness", 0.5f);
-
-        disk.GetComponent<MeshRenderer>().material = diskMaterial;
-        foreach (Transform shatteredDisks in diskShattered.transform)
-        {
-            shatteredDisks.GetComponent<MeshRenderer>().material = diskMaterial;
-        }
+      diskMaterial.color = Color.red;
+      scoreIncrementation = -2;
+    }
+    else
+    {
+      diskMaterial.color = Color.green;
+      scoreIncrementation = 1;
     }
 
-    [ContextMenu("Shatter Disk")]
-    public void ShatterDisk()
+    diskMaterial.SetFloat("_Metallic", 1f);
+    diskMaterial.SetFloat("_Glossiness", 0.5f);
+
+    disk.GetComponent<MeshRenderer>().material = diskMaterial;
+    foreach (Transform shatteredDisks in diskShattered.transform)
     {
-        int randomNumber = Random.Range(0, 3);
-        shatterSounds[randomNumber].PlayOneShot(shatterSounds[randomNumber].clip, 0.5f);
-        diskShattered.transform.position = disk.transform.position;
-        disk.SetActive(false);
-        diskShattered.SetActive(true);
-        DisabilityEvent.instance.IncrementScore(scoreIncrementation);
+      shatteredDisks.GetComponent<MeshRenderer>().material = diskMaterial;
     }
+  }
+
+  [ContextMenu("Shatter Disk")]
+  public void ShatterDisk()
+  {
+    int randomNumber = Random.Range(0, 3);
+    shatterSounds[randomNumber].PlayOneShot(shatterSounds[randomNumber].clip, 0.5f);
+    diskShattered.transform.position = disk.transform.position;
+    disk.SetActive(false);
+    diskShattered.SetActive(true);
+    DisabilityEvent.instance.IncrementScore(scoreIncrementation);
+  }
 }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -13,33 +13,57 @@ public class Shoot : MonoBehaviour
   public AudioSource deagleSound;
   public GameObject casingPrefab;
   public GameObject muzzleFlashPrefab;
+  public bool isRightHand;
 
   [SerializeField] private Animator gunAnimator;
   [SerializeField] private Transform casingExitLocation;
   [Tooltip("Specify time to destory the casing object")][SerializeField] private float destroyTimer = 2f;
   void Update()
   {
-    var rightHandDevices = new List<UnityEngine.XR.InputDevice>();
-    UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.RightHanded, rightHandDevices);
-
-    if (rightHandDevices.Count > 0)
+    var handDevices = new List<UnityEngine.XR.InputDevice>();
+    if (isRightHand)
     {
-      UnityEngine.XR.InputDevice rightController = rightHandDevices[0];
+      UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.RightHanded, handDevices);
 
-      bool triggerButtonPressed;
-      if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerButtonPressed))
+      if (handDevices.Count > 0)
       {
-        if (triggerButtonPressed && Time.time >= lastShotTime + cooldownDuration)
+        UnityEngine.XR.InputDevice rightController = handDevices[0];
+
+        bool triggerButtonPressed;
+        if (rightController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerButtonPressed))
         {
-          ShootBullet();
-          lastShotTime = Time.time;
-          rightController.SendHapticImpulse(0, 0.4f, 0.1f);
+          if (triggerButtonPressed && Time.time >= lastShotTime + cooldownDuration)
+          {
+            ShootBullet();
+            lastShotTime = Time.time;
+            rightController.SendHapticImpulse(0, 0.4f, 0.1f);
+          }
+        }
+      }
+    }
+    else
+    {
+      UnityEngine.XR.InputDevices.GetDevicesWithRole(UnityEngine.XR.InputDeviceRole.LeftHanded, handDevices);
+
+      if (handDevices.Count > 0)
+      {
+        UnityEngine.XR.InputDevice leftController = handDevices[0];
+
+        bool triggerButtonPressed;
+        if (leftController.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerButtonPressed))
+        {
+          if (triggerButtonPressed && Time.time >= lastShotTime + cooldownDuration)
+          {
+            ShootBullet();
+            lastShotTime = Time.time;
+            leftController.SendHapticImpulse(0, 0.4f, 0.1f);
+          }
         }
       }
     }
   }
 
-[ContextMenu("Shoot")]
+  [ContextMenu("Shoot")]
   void ShootBullet()
   {
     gunAnimator.speed = 3.0f;
